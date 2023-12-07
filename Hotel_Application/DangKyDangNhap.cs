20b7a70329ef2ccca1df2c7726f2ff51885a86c9
tuanhuy1104pro@gmail.com <tuanhuy1104pro@gmail.com>
2023-12-07 18:27:27 +0700

    Dang Ky Dang Nhap fomr

--- Hotel_Application/DangKyDangNhap.cs
+++ Hotel_Application/DangKyDangNhap.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Hotel_Application
 {
-    public partial class Form1 : Form
+    public partial class DangKyDangNhap : Form
     {
-        public Form1()
+        public DangKyDangNhap()
         {
             InitializeComponent();
         }
--- Hotel_Application/Program.cs
+++ Hotel_Application/Program.cs
@@ -16,7 +16,7 @@ namespace Hotel_Application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DangKyDangNhap());
         }
     }
 }
